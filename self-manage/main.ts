ce084555ee35a3bb5181ad53b37ac2694a031ee8
Heechul Ryu <ryuhcii@gmail.com>
2022-03-26 09:26:10 -0700

    Add provider_token variable

--- self-manage/main.ts
+++ self-manage/main.ts
@@ -1,6 +1,6 @@
 import { Construct } from "constructs";
 import { App, TerraformStack, TerraformVariable, RemoteBackend } from "cdktf";
-import { TfeProvider, Workspace } from "@cdktf/provider-tfe"
+import { TfeProvider, Workspace, Variable, OrganizationToken } from "@cdktf/provider-tfe"
 
 const hostname = "app.terraform.io";
 
@@ -24,21 +24,41 @@ class MyStack extends TerraformStack {
       },
     });
 
-    new Workspace(this, "tfc_getting_started", {
-      name: "tfc-getting-started",
-      organization,
-      executionMode: "remote",
-      autoApply: true,
-      workingDirectory: "tfc_getting_started",
-      lifecycle: {
-        ignoreChanges: ['vcs_repo'] // following actual provider's stanza
-      }
-      // // manage repo manually (given with nice UI from Terraform Cloud web page) might be practically a lot better until the scale is too big
-      // vcsRepo: {}
+    const token = new OrganizationToken(this, 'org_token', {
+      organization
     })
+
+    createWorkspaceTfcGettingStarted(this, organization, token.token)
   }
 }
 
 const app = new App()
 new MyStack(app, "self-manage");
 app.synth();
+
+function createWorkspaceTfcGettingStarted(scope: Construct, organization: string, token: string) {
+  const workspace = new Workspace(scope, "tfc_getting_started", {
+    name: "tfc-getting-started",
+    organization,
+    executionMode: "remote",
+    autoApply: true,
+    workingDirectory: "tfc_getting_started",
+    lifecycle: {
+      ignoreChanges: ['vcs_repo'] // following actual provider's stanza
+    }
+    // // manage repo manually (given with nice UI from Terraform Cloud web page) might be practically a lot better until the scale is too big
+    // vcsRepo: {}
+  })
+
+  createVariable(scope, workspace.id, 'provider_token', token, true);
+}
+
+function createVariable(scope: Construct, workspace: string, key: string, value: string, sensitive = false) {
+  new Variable(scope, key, {
+    category: 'terraform',
+    sensitive,
+    value,
+    key,
+    workspaceId: workspace
+  })
+}
