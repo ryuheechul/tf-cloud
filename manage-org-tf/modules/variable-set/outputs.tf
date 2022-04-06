464019f07df7792b5e7bbdcce06169e95c297769
Heechul Ryu <ryuhcii@gmail.com>
2022-04-06 13:20:25 -0700

    manage-org-tf: add more debug output

--- manage-org-tf/modules/variable-set/outputs.tf
+++ manage-org-tf/modules/variable-set/outputs.tf
@@ -1,3 +1,7 @@
 output "variable_set" {
   value = tfe_variable_set.varset
 }
+
+output "workspace_ids" {
+  value = tfe_variable_set.varset.workspace_ids
+}
--- manage-org-tf/outputs.tf
+++ manage-org-tf/outputs.tf
@@ -13,3 +13,7 @@ output "debug_local_workspace_bundles" {
 output "debug_local_workspaces_with_tag_exclusive" {
   value = local.workspaces_with_tag_exclusive
 }
+
+output "debug_module_varset_only_for_exclusive_tag_workspace_ids" {
+  value = module.varset_only_for_exclusive_tag.workspace_ids
+}
